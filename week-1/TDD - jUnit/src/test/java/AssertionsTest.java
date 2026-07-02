0482e6e33090c185012c897753ea52973378ab58
sameer512100 <sayyedsameer2106@gmail.com>
2026-07-02 16:16:40 +0530

    week1 done

---
+++ week-1/TDD - jUnit/src/test/java/AssertionsTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions(){
+
+        assertEquals(5,2+3);
+
+        assertTrue(10>5);
+
+        assertFalse(2>5);
+
+        assertNull(null);
+
+        assertNotNull(new Object());
+
+        assertEquals("Hello","Hel"+"lo");
+    }
+}
